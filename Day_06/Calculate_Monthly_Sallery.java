d0e3f0e853464485b12b3d9a1402206d379814f2
sumitsomase <sumitsomase65@gmail.com>
2024-09-19 12:12:40 +0530

    Calculate monthly sallery program6

--- Day_06/Calculate_Monthly_Sallery.java
+++ Day_06/Calculate_Monthly_Sallery.java
@@ -1,48 +1,44 @@
 package Day_06;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class Calculate_Monthly_Sallery {
 
-    public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-        System.out.println("Please enter CTC");  
-		Double ctc=sc.nextDouble(); 
-        double monthlysallery=0;
-        double annualtaxamount=0;
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a CTC: ");     
+        int ctc=sc.nextInt();
+
+        double tax=0;
         
 
         if(ctc<=750000){
-
-            annualtaxamount=0;
-            
+            tax=0;
         }else if(ctc<=1000000){
-            annualtaxamount=(ctc-750000)*.10;
+            tax=(ctc-750000)*.10;
         }else if(ctc<=1500000){
-            annualtaxamount=(250000)*.10+(ctc-1000000)*.20;
+            tax= 250000*.10 + (ctc-1000000)*.20;  
         }else{
-            annualtaxamount=(250000)*.10+(500000)*.20+(ctc-1500000)*.30;
+            tax=(250000*.10) + (500000*.20) + (ctc-1500000)*.30;
         }
-        System.out.println("Annual sallery="+ctc);
-        System.out.println("Annual tax ammount="+annualtaxamount);
 
-        double nontaxablemonthlysalary=ctc/12;
-        System.out.println("monthly income in a non taxable country="+nontaxablemonthlysalary);
+        System.out.println("annual goverment tax="+tax);
+        System.out.println("monthly goverment  tax="+tax/12);
 
-
-        double montlytaxamount=annualtaxamount/12;
-        System.out.println("monthly tax amount="+montlytaxamount);
-
-        monthlysallery=nontaxablemonthlysalary-montlytaxamount;
-
-        System.out.println("monthly income in a taxable country="+monthlysallery);
+        System.out.println("annual sallery in taxable country is = "+(ctc-tax));
+        System.out.println("monthly sallery in taxable country is = "+(ctc-tax)/12);
 
 
+        System.out.println("Annual income in non-taxable country="+ctc);
+        System.out.println("Monthly income in non-taxable country="+ctc/12);
 
-     sc.close();
+        
 
 
+        sc.close();
 
     }
+
+
     
 }
